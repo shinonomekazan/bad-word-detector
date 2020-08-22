dc825075019c5a7fdbab27d190e18c5e2c7806f1
Kaley Kriminger <kaley.kriminger@gmail.com>
2020-08-22 17:38:41 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,99 @@
+export class BadWordDetector {
+	private wordList: string[];
+
+	constructor(wordList: string[]) {
+		this.wordList = wordList;
+	}
+
+	private removeSpaces(word: string): string {
+		return word.replace(/\s+/g, "");
+	}
+
+	private removeNonLetters(word: string): string {
+		return word.replace(/([^\p{L}-]+)/ug, () => "");
+	}
+
+	private containsJapanese(word: string): boolean {
+		const japaneseCharRegex = /[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B/g;
+		
+		if (!japaneseCharRegex.test(word)) return false;
+
+		return true;
+	}
+
+	private hiraganaToKatakana(word: string): string {
+		return word.replace(/[\u3041-\u3096]/g, (letter) => String.fromCharCode(letter.charCodeAt(0) + 0x60));
+	}
+
+	private normalizeWord(word: string): string {
+		const spacesRemoved = this.removeSpaces(word);
+
+		const containsJapaneseOrFullWidthEnglish = this.containsJapanese(spacesRemoved);
+
+		let normalized = spacesRemoved;
+		if (containsJapaneseOrFullWidthEnglish) {
+			const hiraganaToKatakana = this.hiraganaToKatakana(normalized);
+			// 全角英語を半角にする、半角カタカナを全角にする
+			normalized = hiraganaToKatakana.normalize("NFKC");
+		}
+
+		// 全角英語も「containsJapanese」になってしまうので、７１行目でノーマライズされたあとももう一回チェックする
+		const containsJapanese = this.containsJapanese(normalized);
+		if (!containsJapanese) {
+			// "â"を"a"にするとか
+			// NKFDは日本語が入っていると「ド」が「ト」になったりするので使えない
+			normalized = normalized.normalize("NFKD");
+		}
+
+		const specialCharsRemoved = this.removeNonLetters(normalized);
+		if (!specialCharsRemoved.length) throw new Error("Input contains no valid characters");
+
+		return specialCharsRemoved.toLowerCase();
+	}
+
+	private containsExactMatch(word: string): boolean {
+		return this.wordList.includes(word);
+	}
+
+	private containsPartialMatch(word: string): boolean {
+		for (let i = 0; i < word.length - 2; i++) {
+			let maxIndex = i;
+
+			while (maxIndex < word.length) {
+				// This was already confirmed in containsExactMatch
+				if (i === 0 && maxIndex === word.length - 1) {
+					maxIndex++;
+					continue;
+				}
+
+				const toCheck = word.substring(i, maxIndex + 1);
+
+				if (this.wordList.includes(toCheck)) {
+					return true;
+				}
+				maxIndex++;
+			}
+		}
+
+		return false;
+	}
+
+	private containsMatch(word: string): boolean {
+		if (this.containsExactMatch(word)) return true;
+
+		if (this.containsPartialMatch(word)) return true;
+
+		return false;
+	}
+
+	isBad(input: string): boolean {
+		if (!input.length) throw new Error("Input is empty");
+
+		const normalizedWord = this.normalizeWord(input);
+
+		const containsMatch = this.containsMatch(normalizedWord);
+
+		console.log(input, containsMatch);
+		return containsMatch;
+	}
+}
---
+++ src/tests/test.ts
@@ -0,0 +1,73 @@
+import { BadWordDetector } from "../";
+
+// BadWordDetector converts hiragana to katakana when checking, so base list should not contain hiragana
+const blacklistedWordList = [
+  "strawberry",
+  "スイカ",
+  "西瓜",
+	"メロン",
+	"passionfruit",
+	"桃",
+	"苺",
+	"キウィ",
+	"ジュース",
+  "アサイー",
+  "ピザ"
+];
+
+// BadWordDetector should detect all these
+const testBlacklistedInput = [
+  "STRáWBERRY",
+  "   　STRAW🍓 BERRY",
+  "すいか",
+  "すい＊＊＊か",
+  "西瓜",
+  "めろん",
+  "　め　ろ　ん　・・",
+  "ｐássioｎｆｒｕｉｔ",
+  "　桃",
+  "苺~~~~!!",
+  "きうぃ",
+  "じゅーす",
+  "じ🍊ゅ＆＆ーす",
+  "あさいー",
+  "ぴざ",
+];
+
+// BadWordDetector should allow these
+const testOkInput = [
+  "elephant",
+  "blueberry",
+  "berry",
+  "passion",
+  "fruit",
+  "シマウマ",
+  "黒猫",
+  "アイス",
+  "カワウソ",
+  "メロディー",
+  "ひざ",
+  "ひさ",
+  "ピサ",
+  "シューす",
+  "シューズ",
+  "スイーツ",
+  "きう",
+];
+
+
+describe("BadWordDetector", () => {
+  const detector = new BadWordDetector(blacklistedWordList);
+
+  it("Returns true when string contains a blacklisted word", () => {
+    for (const badWord of testBlacklistedInput) {
+      expect(detector.isBad(badWord)).toBe(true);
+    }
+  });
+
+  it("Returns false when string does not contain blacklisted word", () => {
+    for (const okWord of testOkInput) {
+      expect(detector.isBad(okWord)).toBe(false);
+    }
+  });
+});
